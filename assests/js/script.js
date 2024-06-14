a1b9737480fe1be4cb796645b0eaab4905b8ebca
amashamaduwanthi <amashamaduwanthi123@gmail.com>
2024-06-14 11:33:56 +0530

    linked assignments

---
+++ assests/js/script.js
@@ -0,0 +1,7 @@
+const typed=new Typed('.Multi-text',{
+    strings: ['Frontend Developer','Backend Developer','Web Designer'],
+    typeSpeed:100,
+    backSpeed:100,
+    backDelay:1000,
+    loop :true,
+})
